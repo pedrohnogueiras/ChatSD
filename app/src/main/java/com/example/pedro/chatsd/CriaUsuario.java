b14031a66e5d64ce7a95df26e3a78ee97b37bee1
pedro <pedrohnogueiras@hotmail.com>
2016-06-14 23:59:06 -0300

    teste

---
+++ app/src/main/java/com/example/pedro/chatsd/CriaUsuario.java
@@ -0,0 +1,20 @@
+package com.example.pedro.chatsd;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pedro on 14/06/2016.
+ */
+public class CriaUsuario extends AppCompatActivity {
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
