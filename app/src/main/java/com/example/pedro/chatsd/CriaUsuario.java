5f14d00cc79dacec368850a68b3e132781b1f943
pedro <pedrohnogueiras@hotmail.com>
2016-06-15 00:08:42 -0300

    teste

--- app/src/main/java/com/example/pedro/chatsd/CriaUsuario.java
+++ app/src/main/java/com/example/pedro/chatsd/CriaUsuario.java
@@ -2,6 +2,9 @@ package com.example.pedro.chatsd;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 /**
  * Created by pedro on 14/06/2016.
@@ -14,7 +17,10 @@ public class CriaUsuario extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-
+    private EditText nome;
+    private EditText sobrenome;
+    private EditText email;
+    private EditText numero;
+    private Button cadastrar;
 
 }
